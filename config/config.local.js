7ae9d37e0ebb2dd77e102a88ddb1fefdfc25be70
xiedajian <172265972@qq.com>
2018-09-12 18:43:21 +0800

    xdj

--- config/config.local.js
+++ config/config.local.js
@@ -11,43 +11,31 @@
 module.exports = appInfo => {
 	const config = exports = {};
 	
+	// redis单客户端配置
+	config.redis = {
+		client: {
+			port: 6379,          // Redis port
+			// host: '127.0.0.1',   // Redis host
+			host: '47.104.248.84',   // Redis host
+			password: '',
+			db: 0,
+		},
+	}
+	
 	// ORM: sequelize 本地开发时配置 , 单个数据库链接
 	config.sequelize = {
 		delegate: 'model',  // load all models to app.model and ctx.model
 		baseDir: 'model',   // load models from `app/model/*.js`
 		dialect: 'mysql',   // support: mysql, mariadb, postgres, mssql
-		host: '127.0.0.1',
+		// host: '127.0.0.1',
+		host: '47.104.248.84',
 		port: 3306,
-		database: 'egg_demo',
+		database: 'hushangmingjia',
 		username: 'root',
-		password: 'root',
+		// password: 'root',
+		password: '123456',
 	};
 	
-	// ORM: sequelize 配置 , 多数据库链接
-	// config.sequelize = {
-	// 	datasources: [
-	// 		{
-	// 			delegate: 'model',  // load all models to app.model and ctx.model
-	// 			baseDir: 'model',   // load models from `app/model/*.js`
-	// 			dialect: 'mysql',   // support: mysql, mariadb, postgres, mssql
-	// 			host: '127.0.0.1',
-	// 			port: 3306,
-	// 			database: 'egg_demo',
-	// 			username: 'root',
-	// 			password: 'root',
-	// 		},
-	// 		{
-	// 			delegate: 'adminModel', // load all models to app.adminModel and ctx.adminModel
-	// 			baseDir: 'admin_model', // load models from `app/admin_model/*.js`
-	// 			dialect: 'mysql',
-	// 			host: '47.104.248.84',
-	// 			port: 3306,
-	// 			database: 'egg_demo',
-	// 			username: 'root',
-	// 			password: 'root',
-	// 		},
-	// 	],
-	// }
 	
 	
 	return config;
